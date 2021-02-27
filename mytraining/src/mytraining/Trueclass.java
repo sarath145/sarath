6b4d342b8ea3339785a8f4fc1a5fd6a3b2b58a5d
sarath143 <asarathraja95244@gmail.com>
2021-02-27 00:24:28 -0800

    initial commit

---
+++ mytraining/src/mytraining/Trueclass.java
@@ -0,0 +1,36 @@
+package mytraining;
+
+import firstdayclass.feb27;
+
+public class Trueclass {
+	private void first() {
+		System.out.println("my fist");
+			}
+			private void second() {
+			System.out.println("second");	
+			}
+			private void third() {
+		System.out.println("my third");
+			}
+			private void fourth() {
+		System.out.println("fourth");
+
+			}
+		public static void main(String[] args) {
+			Trueclass c = new Trueclass();
+			c.first();
+			c.second();
+			c.third();
+			
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
